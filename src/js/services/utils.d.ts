377544daf8caca7b19b5b5c739bd89301af708ba
laoyu <hilaoyu@qq.com>
2024-08-16 14:32:29 +0800

    fix

--- src/js/services/utils.d.ts
+++ src/js/services/utils.d.ts
@@ -23,7 +23,7 @@ export  interface UtilsStatic  {
 	listenKeyUp(key : string, tag : string, callback : CallableFunction): boolean;
 
 	listenKeyUpRemove(key : string, tag : any): void;
-	buildUrl(url : any, params ?: any): any;
+	buildUrl(url : string, params ?: any): string;
 
 	isUrl(str : string): boolean;
 	inArray(needle : any, arr : Array<any>): boolean;
